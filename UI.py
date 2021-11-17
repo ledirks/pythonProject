fc657fbaba8bc1d5ce8d584239f2c789bc4b64cf
lukedirks <ledirks@iastate.edu>
2021-11-17 15:12:01 -0600

    :(

--- UI.py
+++ UI.py
@@ -4,8 +4,8 @@ from stockfish import Stockfish
 import serial
 import aioconsole
 import asyncio
-stockfish = Stockfish(r"C:\Users\david\Downloads\stockfish_14.1_win_x64_avx2\stockfish_14.1_win_x64_avx2\stockfish_14.1_win_x64_avx2.exe")
 
+stockfish = Stockfish("/usr/local/Cellar/stockfish/14/bin/stockfish")
 
 root = Tk()
 root.title("Chess")
@@ -13,6 +13,7 @@ root.title("Chess")
 
 class Board:
     board: Text
+
     def __init__(self):
         self.board = Text(root, width=50, height=25)
 
@@ -26,23 +27,24 @@ class Board:
     def draw(self):
         # self.canvas.move(self.id, 0, -1)
         # self.canvas.after(10, self.draw)  #(time_delay, method_to_execute)
-        
-        #infinitely print board without blocking other input 
+
+        # infinitely print board without blocking other input
         # self.board.pack(side=LEFT)
-        self.board.delete('1.0',END)
+        self.board.delete('1.0', END)
         self.board.insert('1.0', "  A   B   C   D   E   F   G   H\n")
         self.board.insert('1.0', stockfish.get_board_visual())
         self.board.grid(row=0, column=0)
         self.board.after(1000, self.draw)
 
+
 board = Board()
+
 
 # def displayBoard():
 #     board.insert('1.0', "  A   B   C   D   E   F   G   H\n")
 #     board.insert('1.0', stockfish.get_board_visual())
 
 #     root.update()
-
 
 
 # forfeitButton = Button(text="Forfeit", width=25, height=5, bg="black", fg="white")
@@ -60,19 +62,16 @@ board = Board()
 
 # async wait for console input
 async def getConsoleInput():
-    return await asyncio.sleep(1, result=3)
+    move = str(input("User move"))
+    return [move]
 
 
-def playGame():
+async def playGame():
     board.draw()
-    
+    stockfish.make_moves_from_current_position(await getConsoleInput())
     root.mainloop()
+    return
 
-playGame()
+
+asyncio.run(playGame())
 print('done')
-
-
-
-
-
-
